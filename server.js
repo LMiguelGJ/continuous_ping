547c1a35215fc5b976b1e2474702489e8225fff2
Luis Miguel <l.miguel.gonzalez.j@gmail.com>
2025-01-19 16:27:35 -0400

    adatado para render

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000; // Usa el puerto definido en la variable de entorno
+
+// Definir una ruta básica
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Asegúrate de que está escuchando en 0.0.0.0 para que sea accesible externamente
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App listening on port ${port}`);
+});
