188e2fd98f9b14ac354c39c62686999c43228efb
Luis Miguel <l.miguel.gonzalez.j@gmail.com>
2025-01-19 18:26:00 -0400

    adatado para render log

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// Agregar una nueva ruta para servir los logs
+app.get('/logs', (req, res) => {
+  const fs = require('fs');
+  const logFilePath = '/home/node/logs/server.log'; // Ruta del archivo de logs
+
+  fs.readFile(logFilePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Error al leer el archivo de logs');
+    }
+    res.send(`<pre>${data}</pre>`); // Enviar el contenido del log como respuesta
+  });
+});
+
 // Asegúrate de que está escuchando en 0.0.0.0 para que sea accesible externamente
 app.listen(port, '0.0.0.0', () => {
   console.log(`App listening on port ${port}`);
