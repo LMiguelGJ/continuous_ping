02e544182e9ff75d156cf48060b753db81b8fc20
Luis Miguel <l.miguel.gonzalez.j@gmail.com>
2025-01-19 16:31:22 -0400

    adatado para render

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = process.env.PORT || 4000; // Usa el puerto definido en la variable de entorno
 
