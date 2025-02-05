9d81e303c77bbfc9b415caafa7722de0de3ebdae
LMiguelGJ <142045066+LMiguelGJ@users.noreply.github.com>
2025-02-05 14:35:29 -0400

    Update server.js The last lines

--- server.js
+++ server.js
@@ -1,40 +1,41 @@
-import express from 'express';
-import fs from 'fs'; // Importar el módulo fs
+import express from 'express'; 
+import fs from 'fs';
 const app = express();
 const port = process.env.PORT || 4000; // Usa el puerto definido en la variable de entorno
 
-// Agregar una nueva ruta para servir los logs
 app.get('/', (req, res) => {
   const logFilePath = '/home/node/server.log'; // Ruta del archivo de logs
 
- // Obtener las últimas 100 líneas del log para optimizar rendimiento
-  exec(`tail -n 100 ${logFilePath}`, (err, stdout, stderr) => {
-    if (err || stderr) {
+  fs.readFile(logFilePath, 'utf8', (err, data) => {
+    if (err) {
       return res.status(500).send('Error al leer el archivo de logs');
     }
-    
-    // Modificación para cambiar el fondo a negro y recargar cada 5 segundos
+
+    // Dividimos el archivo en líneas y tomamos solo las últimas 100
+    const lines = data.split('\n');
+    const lastLines = lines.slice(-100).join('\n');
+
     res.send(`
       <html>
         <head>
           <style>
-            body { background-color: black; color: white; }
+            body { background-color: black; color: white; font-family: monospace; }
             pre { white-space: pre-wrap; word-wrap: break-word; }
           </style>
           <script>
             setInterval(() => {
               window.location.reload();
-            }, 50000);
+            }, 5000);
             setInterval(() => {
               window.scrollTo(0, document.body.scrollHeight);
             }, 5000);
           </script>
         </head>
         <body>
-          <pre>${data}</pre>
+          <pre>${lastLines}</pre>
         </body>
       </html>
-    `); // Enviar el contenido del log como respuesta
+    `);
   });
 });
 
